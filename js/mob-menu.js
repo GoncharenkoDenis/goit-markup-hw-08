854febeed3f308c412e0ef65e731bcddaed5c632
Denis <denisgoncarenko18@gmail.com>
2023-01-07 11:05:02 +0200

    ф

--- js/mob-menu.js
+++ js/mob-menu.js
@@ -1,14 +1,16 @@
-(() => {
-  const menuBtnRef = document.querySelector("[data-menu-button]");
-  const mobileMenuRef = document.querySelector("[data-menu]");
+const menuBtnRef = document.querySelector("[data-menu-button]");
+const mobileMenuRef = document.querySelector("[data-menu]");
+menuBtnRef.classList.remove("is-open");
+menuBtnRef.addEventListener("click", () => {
+  const expanded = menuBtnRef.getAttribute("aria-expanded") === "true" || false;
 
-  menuBtnRef.addEventListener("click", () => {
-    const expanded =
-      menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+  menuBtnRef.classList.toggle("is-open");
+  menuBtnRef.setAttribute("aria-expanded", !expanded);
 
-    menuBtnRef.classList.toggle("is-open");
-    menuBtnRef.setAttribute("aria-expanded", !expanded);
+  mobileMenuRef.classList.toggle("is-open");
 
-    mobileMenuRef.classList.toggle("is-open");
-  });
-})();
+  const cross = menuBtnRef.querySelector(".icon-cross");
+  const menu = menuBtnRef.querySelector(".icon-menu");
+  cross.style.display = cross.style.display === "inline" ? "none" : "inline";
+  menu.style.display = menu.style.display === "none" ? "inline" : "none";
+});
